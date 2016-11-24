e003874e0e0ab501aef8f3a131b6e5af78976679
Enda Lee <enda.lee@ittdublin.ie>
2016-11-24 13:11:22 +0000

    Almost Done

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -17,6 +17,16 @@ import models.*;
 
 public class HomeController extends Controller {
 
+    private FormFactory formFactory;
+
+
+
+    @Inject
+    public HomeController(FormFactory f) {
+        this.formFactory = f;
+    }
+
+
     public Result index(String name) {
         return ok(index.render("Welcome to the Home page", name));
     }
@@ -30,6 +40,29 @@ public class HomeController extends Controller {
         // Get list of all categories in ascending order
         List<Product> productsList = Product.findAll();
         return ok(products.render(productsList));
+    }
+
+    public Result addProduct() {
+
+        Form<Product> addProductForm = formFactory.form(Product.class);
+
+        return ok(addProduct.render(addProductForm));
+    }
+
+    public Result addProductSubmit() {
+        Form<Product> newProductForm = formFactory.form(Product.class).bindFromRequest();
+
+        if(newProductForm.hasErrors()){
+            return badRequest(addProduct.render(newProductForm));
+        }
+
+        Product newProduct = newProductForm.get();
+
+        newProduct.save();
+
+        flash("success", "Product " + newProduct.getName() + " has been created");
+
+        return redirect(controllers.routes.HomeController.products());
     }
 
 }
